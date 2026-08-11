10fc12be0814d5c31a032cdec6f38802a179ea17
camillemei <camilleon.official@gmail.com>
2026-08-11 13:43:25 -0500

    personalization, new links, and more functions :)

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function ClickHello() {
     alert("omg a new message?! how cool!!!");
 }
+
+function showTime() {
+ const time = new Date();
+ let hours = time.getHours();
+ let minutes = time.getMinutes();
+ let seconds = time.getSeconds();
+ const amPm = hours >= 12 ? "PM" : "AM";
+ hours = hours % 12 || 12;
+ hours = hours < 10 ? "0" + hours : hours;
+ minutes = minutes < 10 ? "0" + minutes : minutes;
+ seconds = seconds < 10 ? "0" + seconds : seconds;
+ const currentTime = `${hours}:${minutes}:${seconds} ${amPm}`;
+ document.getElementById("clock").innerText = currentTime;
+}
+setInterval(showTime, 1000);
+showTime();
